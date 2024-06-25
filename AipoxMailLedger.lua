46efc718c1e66bdae147f22cefeb88df589468e3
Alberto Perez <apo0106@gmail.com>
2024-06-25 10:55:40 +0200

    small fix to filter aution cancelled & auction expired mails

--- AipoxMailLedger.lua
+++ AipoxMailLedger.lua
@@ -24,10 +24,10 @@ function RegisterMails()
     print("Registrando correos...")
     local numItems, totalItems = GetInboxNumItems()
     local packageIcon, stationeryIcon, sender, subject, money, CODAmount, daysLeft, itemCount, wasRead, wasReturned, text, isGM = GetInboxHeaderInfo(1) -- Assuming we only process the first item
-    if not string.find(subject, "Auction cancelled:") then
+    if not string.find(subject, "Auction cancelled:") or not string.find(subject, "Auction expired:") then
         local bodyText = GetInboxInvoiceInfo(1)
         local invoiceType, itemName, playerName, bid, buyout, deposit, consignment = GetInboxInvoiceInfo(1)
-        if sender and (sender == "Horde Auction House" or sender == "Alliance Auction House") and subject then
+        if sender and (sender == "Horde Auction House" or sender == "Alliance Auction House") and subject and (string.find(subject, "Auction won") or string.find(subject, "Auction successful")) then
             local key = sender .. "-" .. subject .. "-" .. tostring(1)
             AipoxMailLedgerDB[key] = {
                 timestamp = time(),
